96710d40fe56664d8be0b680362bfbaf5ddee8a9
pablovisual <pabloarmandosandoval@gmail.com>
2023-02-24 19:00:33 +0000

    code challenge complete

--- src/main/java/ReturnLongestString.java
+++ src/main/java/ReturnLongestString.java
@@ -8,6 +8,23 @@ public class ReturnLongestString {
      * @return the longest String within arr. You can assume that there are no ties for the longest string.
      */
     public String longest(String[] arr){
-        return null;
+        String longString = new String();
+        int counter = 0;
+        return longestString(arr, counter, longString);
+    }
+
+    public String longestString(String[] arr, int counter, String longString) {
+        if (arr.length == 0 || arr.length == 1) {
+            longString = arr.length == 0 ? "" : arr[counter];
+            return longString;
+        }
+            
+        if(counter == arr.length)
+            return longString;
+        
+        else if(longString.length() < arr[counter].length())
+            longString = arr[counter];
+        
+        return longestString(arr, ++counter, longString);
     }
 }
